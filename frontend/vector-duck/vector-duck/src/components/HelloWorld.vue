84130736d720a9636b5ebb83e7cd89edb02c97fc
Rafael Muselmann <rafael.muselmann@oth-regensburg.de>
2019-05-25 10:12:26 +0200

    Adds websocket functionality

--- frontend/vector-duck/vector-duck/src/components/HelloWorld.vue
+++ frontend/vector-duck/vector-duck/src/components/HelloWorld.vue
@@ -5,22 +5,25 @@
         <h2 class="headline font-weight-bold mb-3">Steuerung</h2>
 
         <v-layout justify-center style="flex-flow:column" class="layout">
-          <v-btn>
+          <v-btn @click="forward()">
             <v-icon>arrow_upward</v-icon>
           </v-btn>
           <div>
-            <v-btn>
+            <v-btn @click="backward()">
               <v-icon>arrow_back</v-icon>
             </v-btn>
-            <v-btn>
-            <v-icon>replay</v-icon>
-          </v-btn>
-            <v-btn>
+            <v-btn @click="gyro()">
+              <v-icon>replay</v-icon>
+            </v-btn>
+            <v-btn @canplay="forward()">
               <v-icon>arrow_forward</v-icon>
             </v-btn>
           </div>
-          <v-btn>
+          <v-btn @click="backwards()">
             <v-icon>arrow_downward</v-icon>
+          </v-btn>
+          <v-btn @click="stop()">
+            <v-icon>stop</v-icon>
           </v-btn>
         </v-layout>
       </v-flex>
@@ -39,8 +42,24 @@ export default {
   methods: {
     add() {
       this.websocket.send("asdf");
-
-      // this.$socket.emit("add", { a: 5, b: 3 });
+    },
+    forward() {
+      this.websocket.send("F");
+    },
+    backwards() {
+      this.websocket.send("B");
+    },
+    left() {
+      this.websocket.send("L");
+    },
+    right() {
+      this.websocket.send("R");
+    },
+    gyro() {
+      this.websocket.send("X");
+    },
+    stop() {
+      this.websocket.send("S");
     }
   }
 };
