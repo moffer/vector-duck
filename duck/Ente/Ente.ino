c784f970affd9c4d340d69e5eb6a644a2c25bbf8
Simon-Liebig <simon.liebig@beliebig.eu>
2019-05-25 09:21:49 +0200

    Ente mit gyro funktion auf x

--- duck/Ente/Ente.ino
+++ duck/Ente/Ente.ino
@@ -4,6 +4,8 @@
 #include <ESP8266WebServer.h>
 #include <WebSocketsServer.h>
 
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+
 // WLAN SSID Define
 #ifndef APSSID
 #define APSSID "EnteEnteEnte"
@@ -29,6 +31,23 @@
 #define motorA2 D2  //Achtung das ist kein PWM pin 
 #define motorB1 D6
 #define motorB2 D7
+
+
+
+  #define SDA D4
+  #define SCL D5
+  
+
+//*************************i2c variablen und gyro zeug
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z, init_gyro_z;; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+char tmp_str[7]; // temporary variable used in convert function
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
 
 
 /* Set these to your desired credentials. */
@@ -60,12 +79,28 @@ void handleRoot() {
 
 
 
+void check_gyro ();
+
+
+
 void setup() {
   // Set pins as outputs:
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
   pinMode(motorB2, OUTPUT);  
+
+
+//I2C init 
+    Wire.begin(SDA,SCL);
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+// gyro startwert Hier sollte vielleicht noch gemittelt werden
+  check_gyro();
+  init_gyro_z =gyro_z;
   
   delay(1000);
   // Initialize serial communication at 115200 bits per second:
@@ -178,6 +213,40 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
         digitalWrite(motorA1, LOW);  digitalWrite(motorA2, LOW); 
         digitalWrite(motorB1, LOW);  digitalWrite(motorB2, LOW);
     }
+    /************************Gyro Regelung*****************************/
+  //If state is equal with letter 'X', starts the gyro Regelung
+    else if (payload[0] == 'X'){
+        forward();
+//  Serial.print("gogogogog");
+ // Serial.println();
+  delay(1000);
+check_gyro();
+
+    while(gyro_z > init_gyro_z) //rechtskorrektur
+    {
+          digitalWrite(motorA1, HIGH);digitalWrite(motorA2, LOW); //rechts
+        digitalWrite(motorB1, LOW);     digitalWrite(motorB2, HIGH);   //rechts
+    check_gyro();
+    //Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));Serial.print("rechts");Serial.println();
+    delay(100);
+    }
+    
+    forward(); //etwasgrade fahren
+  //Serial.print("gogogogog");
+  //Serial.println();
+    delay(1000);
+    check_gyro();
+
+    
+    while(gyro_z < init_gyro_z) //linkskorrektur
+    {
+            digitalWrite(motorA1, LOW);     digitalWrite(motorA2, HIGH); //links
+        digitalWrite(motorB1, HIGH);digitalWrite(motorB2, LOW); //links
+      check_gyro();
+   //   Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));Serial.print("links");Serial.println();
+      delay(100);
+    }
+    }
     break;
   }
 }
@@ -190,4 +259,23 @@ void startWebSocket()
     webSocket.onEvent(webSocketEvent); // if there's an incomming websocket message, go to function 'webSocketEvent'
 //    webSocket.enableHeartbeat(15000, 3000, 2);
     Serial.println("WebSocket server started.");
+}
+
+
+
+void check_gyro ()
+{
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
 }
